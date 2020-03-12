daeb0a7ae634d90c42c20090445791f9a0c80907
mammonny <54335870+mammonny@users.noreply.github.com>
2020-03-12 19:34:05 +0100

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -32,7 +32,7 @@ export default {
   },
   data() {
     return {
-      description: 'The ultimate static generated documentation theme for the JAM-stack'
+      description: 'La documentación definitiva para espiar un celular'
     }
   },
   metaInfo() {
